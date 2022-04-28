7d26d7fd20acb8b8c52c10f434c69c8e0475baed
Clara Quer <claraperez.cm@gmail.com>
2022-04-29 00:48:07 +0200

    thursday

--- p5.sql
+++ p5.sql
@@ -1,30 +1,14 @@
 USE p5;
 
-SELECT *
-FROM p5_score;
-
-SELECT COUNT(DISTINCT(country_name))
-FROM in17;
-
-SELECT COUNT(DISTINCT indicator_name)
-FROM indicator;
-
-SELECT COUNT(DISTINCT indicator_name)
-FROM dimensions;
-
-SELECT i.continent, i.region, p.iso3, i.country_name, i.indicator_id, i.value, p.democ, p.autoc, p.polity, id.indicator_name
+SELECT i.continent, i.region, p.iso3, i.country_name, i.indicator_id, i.value, p.democ, p.autoc, p.polity, id.indicator_name, d.dimension
 FROM in17 i
-	LEFT JOIN p5_score p
-		ON i.country_name = p.country
-	JOIN indicator_id
+    LEFT JOIN p5_score p
+        ON i.country_name = p.country
+    JOIN indicator id
+        USING (indicator_id)
+	LEFT JOIN dimensions d
 		USING (indicator_id)
-	 
+GROUP BY country_name, indicator_id
+ORDER BY country_name
+        
 ;
-
-SELECT *
-FROM in17 i
-	JOIN p5_score p
-		ON i.country_name = p.country
-	JOIN indicator id
-		USING (indicator_id)
-WHERE indicator_id = 177006;
